616096ef6fe09de4bd920bb48245defe6e6f7739
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-08-01 00:22:40 +0530

    getting img

--- web.py
+++ web.py
@@ -5,5 +5,5 @@ res = requests.get('https://poojaseenu1999.github.io/epiczoowild/')
 soup = bs4.BeautifulSoup(res.text, 'lxml')
 
 
-for i in soup.select('h1'):
-    print(i.text)
+for i in soup.select('img'):
+    print(i)
